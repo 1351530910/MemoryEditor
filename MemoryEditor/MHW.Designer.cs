242ff32f1734a628e03bbb1c922b5a7eee96ed8a
Chen He <chen.he2@mail.mcgill.ca>
2019-07-22 21:01:43 -0400

    monster hunter world editor

--- MemoryEditor/MHW.Designer.cs
+++ MemoryEditor/MHW.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.gold_lb = new System.Windows.Forms.Label();
             this.gold_tb = new System.Windows.Forms.TextBox();
             this.SP_tb = new System.Windows.Forms.TextBox();
@@ -38,6 +39,18 @@
             this.exp_lb = new System.Windows.Forms.Label();
             this.Scan = new System.Windows.Forms.Button();
             this.save = new System.Windows.Forms.Button();
+            this.refresh_btn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lockedDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.itemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.set_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // gold_lb
@@ -65,14 +78,14 @@
             // 
             // level_tb
             // 
-            this.level_tb.Location = new System.Drawing.Point(386, 10);
+            this.level_tb.Location = new System.Drawing.Point(60, 37);
             this.level_tb.Name = "level_tb";
             this.level_tb.Size = new System.Drawing.Size(100, 21);
             this.level_tb.TabIndex = 3;
             // 
             // exp_tb
             // 
-            this.exp_tb.Location = new System.Drawing.Point(539, 10);
+            this.exp_tb.Location = new System.Drawing.Point(220, 37);
             this.exp_tb.Name = "exp_tb";
             this.exp_tb.Size = new System.Drawing.Size(100, 21);
             this.exp_tb.TabIndex = 4;
@@ -89,7 +102,7 @@
             // level_lb
             // 
             this.level_lb.AutoSize = true;
-            this.level_lb.Location = new System.Drawing.Point(339, 16);
+            this.level_lb.Location = new System.Drawing.Point(13, 43);
             this.level_lb.Name = "level_lb";
             this.level_lb.Size = new System.Drawing.Size(35, 12);
             this.level_lb.TabIndex = 6;
@@ -98,7 +111,7 @@
             // exp_lb
             // 
             this.exp_lb.AutoSize = true;
-            this.exp_lb.Location = new System.Drawing.Point(494, 16);
+            this.exp_lb.Location = new System.Drawing.Point(175, 43);
             this.exp_lb.Name = "exp_lb";
             this.exp_lb.Size = new System.Drawing.Size(23, 12);
             this.exp_lb.TabIndex = 7;
@@ -106,7 +119,7 @@
             // 
             // Scan
             // 
-            this.Scan.Location = new System.Drawing.Point(658, 8);
+            this.Scan.Location = new System.Drawing.Point(345, 10);
             this.Scan.Name = "Scan";
             this.Scan.Size = new System.Drawing.Size(75, 23);
             this.Scan.TabIndex = 8;
@@ -116,7 +129,7 @@
             // 
             // save
             // 
-            this.save.Location = new System.Drawing.Point(740, 8);
+            this.save.Location = new System.Drawing.Point(345, 35);
             this.save.Name = "save";
             this.save.Size = new System.Drawing.Size(75, 23);
             this.save.TabIndex = 9;
@@ -124,11 +137,103 @@
             this.save.UseVisualStyleBackColor = true;
             this.save.Click += new System.EventHandler(this.Save_Click);
             // 
+            // refresh_btn
+            // 
+            this.refresh_btn.Location = new System.Drawing.Point(426, 8);
+            this.refresh_btn.Name = "refresh_btn";
+            this.refresh_btn.Size = new System.Drawing.Size(75, 23);
+            this.refresh_btn.TabIndex = 11;
+            this.refresh_btn.Text = "refresh";
+            this.refresh_btn.UseVisualStyleBackColor = true;
+            this.refresh_btn.Click += new System.EventHandler(this.Refresh_btn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(307, 20);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.countDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.lockedDataGridViewCheckBoxColumn});
+            this.dataGridView1.DataSource = this.itemBindingSource;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 91);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(548, 333);
+            this.dataGridView1.TabIndex = 14;
+
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // countDataGridViewTextBoxColumn
+            // 
+            this.countDataGridViewTextBoxColumn.DataPropertyName = "count";
+            this.countDataGridViewTextBoxColumn.HeaderText = "count";
+            this.countDataGridViewTextBoxColumn.Name = "countDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // lockedDataGridViewCheckBoxColumn
+            // 
+            this.lockedDataGridViewCheckBoxColumn.DataPropertyName = "locked";
+            this.lockedDataGridViewCheckBoxColumn.HeaderText = "locked";
+            this.lockedDataGridViewCheckBoxColumn.Name = "lockedDataGridViewCheckBoxColumn";
+            // 
+            // itemBindingSource
+            // 
+            this.itemBindingSource.DataSource = typeof(MemoryEditor.Item);
+            // 
+            // set_btn
+            // 
+            this.set_btn.Location = new System.Drawing.Point(426, 37);
+            this.set_btn.Name = "set_btn";
+            this.set_btn.Size = new System.Drawing.Size(75, 23);
+            this.set_btn.TabIndex = 15;
+            this.set_btn.Text = "set";
+            this.set_btn.UseVisualStyleBackColor = true;
+            this.set_btn.Click += new System.EventHandler(this.Set_btn_Click);
+            // 
             // MHW
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 53);
+            this.ClientSize = new System.Drawing.Size(575, 460);
+            this.Controls.Add(this.set_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.refresh_btn);
             this.Controls.Add(this.save);
             this.Controls.Add(this.Scan);
             this.Controls.Add(this.exp_lb);
@@ -141,10 +246,21 @@
             this.Controls.Add(this.gold_lb);
             this.Name = "MHW";
             this.Text = "MHW";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
+
+       
+
+
+
+
+
+
+
 
         #endregion
 
@@ -158,5 +274,15 @@
         private System.Windows.Forms.Label exp_lb;
         private System.Windows.Forms.Button Scan;
         private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button refresh_btn;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn lockedDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.BindingSource itemBindingSource;
+        private System.Windows.Forms.Button set_btn;
     }
 }
--- MemoryEditor/MHW.cs
+++ MemoryEditor/MHW.cs
@@ -8,27 +8,70 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Threading;
 
 namespace MemoryEditor
 {
+    
     public partial class MHW : Form
     {
-        Process mhw;
+        Process mhw = null;
         IntPtr handle;
         ulong baseAddress;
-
-        int gold = 1161630;
-        int sp = 56865;
+        
+        int gold;
+        int sp;
         int level;
         int exp;
 
+        Item[] inventoryItems = new Item[24];
+        Item[] inventoryAmmo = new Item[16];
+        Item[] stockItems = new Item[150];
+        Item[] stockAmmo = new Item[100];
+        Item[] stockMaterials = new Item[350];
+        Item[] stockGems = new Item[150];
+
+        static List<Item> locks = new List<Item>();
         public MHW()
         {
             InitializeComponent();
-            mhw = Process.GetProcessesByName("MonsterHunterWorld").FirstOrDefault() ?? throw new Exception("process not found");
-            handle = mhw.Handle;
+            dataGridView1.AllowUserToAddRows = false;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             gold_tb.Text = gold.ToString();
             SP_tb.Text = sp.ToString();
+            comboBox1.Items.Add("Inventory Items");
+            comboBox1.Items.Add("Inventory Ammo");
+            comboBox1.Items.Add("Stock Items");
+            comboBox1.Items.Add("Stock Ammo");
+            comboBox1.Items.Add("Stock Materials");
+            comboBox1.Items.Add("Stock Gems");
+            comboBox1.SelectedIndex = 2;
+            new Thread(() => checklock(this)).Start();
+            this.dataGridView1.CellValueChanged += DataGridView1_CellValueChanged;
+        }
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+            System.Environment.Exit(0);
+        }
+
+        private void checklock(Form a)
+        {
+            while (true)
+            {
+                for (int i = 0; i < locks.Count; i++)
+                {
+                    try
+                    {
+                        var item = locks[i];
+                        ProcessIO.WriteInt32(handle, item.address, item.id);
+                        ProcessIO.WriteInt32(handle, item.address + 4, item.count);
+                    }
+                    catch (Exception) { }
+                }
+
+                Thread.Sleep(1000);
+            }
         }
 
         private void update()
@@ -37,10 +80,118 @@ namespace MemoryEditor
             SP_tb.Text = sp.ToString();
             exp_tb.Text = exp.ToString();
             level_tb.Text = level.ToString();
+            ComboBox1_SelectedIndexChanged(null, null);
+        }
+        private void DataGridView1_CellValueChanged(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+            if (mhw==null)
+            {
+                return;
+            }
+            if (e.ColumnIndex == 4)
+            {
+                if ((bool)dataGridView1.CurrentCell.EditedFormattedValue)
+                {
+                    ((Item[])dataGridView1.DataSource)[e.RowIndex].locked = true;
+                    locks.Add(((Item[])dataGridView1.DataSource)[e.RowIndex]);
+
+                }
+                else
+                {
+                    locks.Remove(((Item[])dataGridView1.DataSource)[e.RowIndex]);
+                    ((Item[])dataGridView1.DataSource)[e.RowIndex].locked = false;
+                }
+            }
+            if (e.ColumnIndex == 2 && ((Item[])dataGridView1.DataSource)[e.RowIndex].locked == false)
+            {
+                var item = ((Item[])dataGridView1.DataSource)[e.RowIndex];
+                ProcessIO.WriteInt32(handle, item.address + 4, int.Parse((string)dataGridView1.CurrentCell.EditedFormattedValue));
+                ((Item[])dataGridView1.DataSource)[e.RowIndex].count = int.Parse((string)dataGridView1.CurrentCell.EditedFormattedValue);
+            }
+            if (e.ColumnIndex == 0 && ((Item[])dataGridView1.DataSource)[e.RowIndex].locked == false)
+            {
+                var item = ((Item[])dataGridView1.DataSource)[e.RowIndex];
+                ProcessIO.WriteInt32(handle, item.address, int.Parse((string)dataGridView1.CurrentCell.EditedFormattedValue));
+                ((Item[])dataGridView1.DataSource)[e.RowIndex].id = int.Parse((string)dataGridView1.CurrentCell.EditedFormattedValue);
+            }
+        }
+        private void DataGridView1_CellEndEdit(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+            
+        }
+        private void DataGridView1_CellValidating(object sender, System.Windows.Forms.DataGridViewCellValidatingEventArgs e)
+        {
+            if (e.ColumnIndex == 4)
+            {
+                if ((bool)e.FormattedValue)
+                {
+                    locks.Add(((Item[])dataGridView1.DataSource)[e.RowIndex]);
+                }
+                else
+                {
+                    locks.Remove(((Item[])dataGridView1.DataSource)[e.RowIndex]);
+                }
+            }
+            if (e.ColumnIndex < 3 && ((Item[])dataGridView1.DataSource)[e.RowIndex].locked==false)
+            {
+                var item = ((Item[])dataGridView1.DataSource)[e.RowIndex];
+                ProcessIO.WriteInt32(handle, item.address, item.id);
+                ProcessIO.WriteInt32(handle, item.address + 4, item.count);
+            }
+            
+            return;
+        }
+        private void refreshMem()
+        {
+            gold = ProcessIO.ReadInt32(handle, baseAddress);
+            sp = ProcessIO.ReadInt32(handle, baseAddress + 4);
+            exp = ProcessIO.ReadInt32(handle, baseAddress + 8);
+            level = ProcessIO.ReadInt32(handle, baseAddress - 4);
+
+            for (ulong i = 0; i < (ulong)inventoryItems.Length; i++)
+            {
+                inventoryItems[i].address = baseAddress + 0x1945c + i * 0x10;
+                inventoryItems[i].id = ProcessIO.ReadInt32(handle, inventoryItems[i].address);
+                inventoryItems[i].count = ProcessIO.ReadInt32(handle, inventoryItems[i].address + 4);
+            }
+            for (ulong i = 0; i < (ulong)inventoryAmmo.Length; i++)
+            {
+                inventoryAmmo[i].address = baseAddress + 0x195dc + i * 0x10;
+                inventoryAmmo[i].id = ProcessIO.ReadInt32(handle, inventoryAmmo[i].address);
+                inventoryAmmo[i].count = ProcessIO.ReadInt32(handle, inventoryAmmo[i].address + 4);
+            }
+            for (ulong i = 0; i < (ulong)stockItems.Length; i++)
+            {
+                stockItems[i].address = baseAddress + 0x19dd4 + i * 0x10;
+                stockItems[i].id = ProcessIO.ReadInt32(handle, stockItems[i].address);
+                stockItems[i].count = ProcessIO.ReadInt32(handle, stockItems[i].address + 4);
+            }
+            for (ulong i = 0; i < (ulong)stockAmmo.Length; i++)
+            {
+                stockAmmo[i].address = baseAddress + 0x1aa54 + i * 0x10;
+                stockAmmo[i].id = ProcessIO.ReadInt32(handle, stockAmmo[i].address);
+                stockAmmo[i].count = ProcessIO.ReadInt32(handle, stockAmmo[i].address + 4);
+            }
+            for (ulong i = 0; i < (ulong)stockMaterials.Length; i++)
+            {
+                stockMaterials[i].address = baseAddress + 0x1b6d4+i * 0x10;
+                stockMaterials[i].id = ProcessIO.ReadInt32(handle, stockMaterials[i].address);
+                stockMaterials[i].count = ProcessIO.ReadInt32(handle, stockMaterials[i].address+4);
+            }
+            for (ulong i = 0; i < (ulong)stockGems.Length; i++)
+            {
+                stockGems[i].address = baseAddress + 0x1e8d4 + i * 0x10;
+                stockGems[i].id = ProcessIO.ReadInt32(handle, stockGems[i].address);
+                stockGems[i].count = ProcessIO.ReadInt32(handle, stockGems[i].address + 4);
+            }
+
+            update();
         }
 
         private void Scan_Click(object sender, EventArgs e)
         {
+            mhw = Process.GetProcessesByName("MonsterHunterWorld").FirstOrDefault() ?? throw new Exception("process not found");
+            handle = mhw.Handle;
             gold = int.Parse(gold_tb.Text);
             sp = int.Parse(SP_tb.Text);
 
@@ -48,9 +199,9 @@ namespace MemoryEditor
             UIntPtr[] addresses = new UIntPtr[1];
             var n = ProcessIO.MemSearch(handle, BitConverter.GetBytes(keyword), 8, addresses, 1);
             if (n >= 1) baseAddress = addresses[0].ToUInt64();
-            exp = ProcessIO.ReadInt32(handle, baseAddress + 8);
-            level = ProcessIO.ReadInt32(handle, baseAddress - 4);
-            update();
+
+            refreshMem();
+
         }
 
         private void Save_Click(object sender, EventArgs e)
@@ -65,5 +216,65 @@ namespace MemoryEditor
             ProcessIO.WriteInt32(handle, baseAddress +8, exp);
         }
 
+
+        private void Refresh_btn_Click(object sender, EventArgs e)
+        {
+            refreshMem();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            if (mhw==null)
+                return;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    dataGridView1.DataSource = inventoryItems;
+                    return;
+                case 1:
+                    dataGridView1.DataSource = inventoryAmmo;
+                    return;
+                case 2:
+                    dataGridView1.DataSource = stockItems;
+                    return;
+                case 3:
+                    dataGridView1.DataSource = stockAmmo;
+                    return;
+                case 4:
+                    dataGridView1.DataSource = stockMaterials;
+                    return;
+                case 5:
+                    dataGridView1.DataSource = stockGems;
+                    return;
+                default:
+                    break;
+            }
+        }
+
+        private void BindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Set_btn_Click(object sender, EventArgs e)
+        {
+            gold = int.Parse(gold_tb.Text);
+            sp = int.Parse(SP_tb.Text);
+            exp = int.Parse(exp_tb.Text);
+            level = int.Parse(level_tb.Text);
+            ProcessIO.WriteInt32(handle, baseAddress, gold);
+            ProcessIO.WriteInt32(handle, baseAddress + 4, sp);
+            ProcessIO.WriteInt32(handle, baseAddress + 8, exp);
+            ProcessIO.WriteInt32(handle, baseAddress - 4, level);
+        }
+    }
+    public struct Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int count { get; set; }
+        public ulong address { get; set; }
+        public bool locked { get; set; }
     }
 }
